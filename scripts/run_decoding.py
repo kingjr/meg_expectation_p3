a12db7c4576c2cae7223f7fecb06a9c156ea5bfe
kingjr <jeanremi.king+github@gmail.com>
2015-06-26 12:33:25 -0400

    WIP: run_decoding

--- scripts/run_decoding.py
+++ scripts/run_decoding.py
@@ -1,116 +1,99 @@
 import os.path as op
 import numpy as np
+# import pickle
 import mne
-from mne.io import Raw
-from mne.io.pick import _picks_by_type as picks_by_type
-from mne.preprocessing import read_ica
-from mne.viz import plot_drop_log
 from mne.decoding import GeneralizationAcrossTime
+
+from meeg_preprocessing.utils import setup_provenance
 from toolbox.jr_toolbox.utils import resample_epochs, decim
-
-import pickle
-import warnings
-
-from p300.conditions import get_events
-
-from meeg_preprocessing.utils import setup_provenance, set_eog_ecg_channels
 
 from scripts.config import (
     data_path,
-    subjects,
-    results_dir,
-    events_fname_filt_tmp,
     epochs_params,
     open_browser,
-    contrasts,
-    decoding_preproc,
-    decoding_params
+    subjects,
+    preproc,
+    analyses,
+    results_dir,
+    epochs_types
 )
+from p300.conditions import get_events
 
 
-report, run_id, results_dir, logger = setup_provenance(script=__file__,
-                                                       results_dir=results_dir)
+report, run_id, _, logger = setup_provenance(
+    script=__file__, results_dir=results_dir)
 
-mne.set_log_level('INFO')
+from itertools import product
 
-for subject in subjects:
-
+for subject, epoch_params, epoch_type in product(subjects, epochs_params,
+                                                 epochs_types):
     # Extract events from mat file
     bhv_fname = op.join(data_path, 'behavior',
                         '{}_behaviorMEG.mat'.format(subject[-2:]))
+    eptyp_name = epoch_params['name'] + epoch_type
+    print(eptyp_name)
 
+    # Get MEG data
+    epo_fname = op.join(data_path, 'MEG', subject,
+                        '{}-{}-epo.fif'.format(eptyp_name, subject))
+    epochs = mne.read_epochs(epo_fname)
+    # Get events specific to epoch definition (stim or motor lock)
+    events = get_events(epochs.events)
 
-    # Apply contrast on each type of epoch
-    all_epochs = [[]] * len(epochs_params)
-    for epoch_params, preproc in zip(epochs_params, decoding_preproc):
-        ep_name = epoch_params['name']
+    # preprocess data for memory issue
+    if 'resample' in preproc.keys():
+        epochs = resample_epochs(epochs, preproc['resample'])
+    if 'decim' in preproc.keys():
+        epochs = decim(epochs, preproc['decim'])
+    if 'crop' in preproc.keys():
+        epochs.crop(preproc['crop']['tmin'],
+                    preproc['crop']['tmax'])
 
-        # Get MEG data
-        epo_fname = op.join(data_path, 'MEG', subject,
-                            '{}-{}-epo.fif'.format(ep_name, subject))
-        epochs = mne.read_epochs(epo_fname)
-        events = get_events(epochs.events)
+    # Apply each analysis
+    for analysis in analyses:
+        logger.info('%s: %s: %s: %s' % (subject, epoch_params['name'], epoch_type,
+                                        analysis['name']))
+        # check if nested analysis
+        if isinstance(analysis['condition'], list):
+            continue
+        query, condition = analysis['query'], analysis['condition']
+        sel = range(len(events)) if query is None \
+            else events.query(query).index
+        sel = [ii for ii in sel if ~np.isnan(events[condition][sel][ii])]
+        y = np.array(events[condition], dtype=np.float32)
 
-        # preprocess data for memory issue
-        if 'resample' in preproc.keys():
-            epochs = resample_epochs(epochs, preproc['resample'])
-        if 'decim' in preproc.keys():
-            epochs = decim(epochs, preproc['decim'])
-        if 'crop' in preproc.keys():
-            epochs.crop(preproc['crop']['tmin'],
-                        preproc['crop']['tmax'])
+        print analysis['name'], np.unique(y[sel]), len(sel)
 
-        # Apply each contrast
-        for contrast in contrasts:
-            # Find excluded trials
-            exclude = np.any([events[x['cond']]==ii
-                                for x in contrast['exclude']
-                                    for ii in x['values']],
-                            axis=0)
+        if len(sel) == 0:
+            logger.warning('%s: no epoch in %s %s for %s.' % (
+                subject, epoch_params['name'], epoch_type, analysis['name']))
+            continue
 
-            # Select condition
-            include = list()
-            cond_name = contrast['include']['cond']
-            for value in contrast['include']['values']:
-                # Find included trials
-                include.append(events[cond_name]==value)
-            sel = np.any(include,axis=0) * (exclude==False)
-            sel = np.where(sel)[0]
+        # Apply analysis
+        gat = GeneralizationAcrossTime(clf=analysis['clf'],
+                                       cv=analysis['cv'],
+                                       scorer=analysis['scorer'],
+                                       n_jobs=-1)
+        gat.fit(epochs[sel], y=y[sel])
+        gat.score(epochs[sel], y=y[sel])
 
-            # reduce number or trials if too many
-            if len(sel) > 400:
-                import random
-                random.shuffle(sel)
-                sel = sel[0:400]
+        # Save analysis
+        # TODO !
+        #
+        # # Save classifier results
+        # with open(pkl_fname, 'wb') as f:
+        #     pickle.dump([gat, analysis, sel, events], f)
 
-            if len(sel) == 0:
-                warnings.warn('%s: no epoch in %s for %s.' % (subject, ep_name,
-                                                              contrast['name']))
-                continue
+        # Plot
+        fig = gat.plot_diagonal(show=False)
+        report.add_figs_to_section(fig, '%s %s %s %s: (diagonal)' % (
+            subject, epoch_params['name'], epoch_type, analysis['name']),
+            analysis['name'])
 
-            y = np.array(events[cond_name].tolist())
-
-            # Apply contrast
-            gat = GeneralizationAcrossTime(**decoding_params)
-            # gat = GeneralizationAcrossTime(n_jobs=-1)
-            gat.fit(epochs[sel], y=y[sel])
-            gat.score(epochs[sel], y=y[sel])
-
-            # Plot
-            fig = gat.plot_diagonal(show=False)
-            report.add_figs_to_section(fig, ('%s %s: %s'
-                % (subject, ep_name, contrast['name'])), subject)
-
-            fig = gat.plot(show=False)
-            report.add_figs_to_section(fig, ('%s %s: %s'
-                % (subject, ep_name, contrast['name'])), subject)
-
-            # Save contrast
-            pkl_fname = op.join(data_path, 'MEG', subject,
-                                '%s-%s-decod_%s.pickle' % (ep_name, subject,
-                                contrast['name']))
-
-            with open(pkl_fname, 'w') as f:
-                pickle.dump([gat, contrast], f)
+        fig = gat.plot(vmin=np.min(gat.scores_),
+                       vmax=np.max(gat.scores_), show=False)
+        report.add_figs_to_section(fig, '%s %s %s %s: GAT' % (
+            subject, epoch_params['name'], epoch_type, analysis['name']),
+            analysis['name'])
 
 report.save(open_browser=open_browser)
