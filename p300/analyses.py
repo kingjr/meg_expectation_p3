715ee4d47a8c282099524dfe1119e3719525326b
Gabriela Meade <meade.gabriela@gmail.com>
2015-09-08 22:46:07 -0700

    FIX: contrast definitions, but still need to double check

--- p300/analyses.py
+++ p300/analyses.py
@@ -46,7 +46,7 @@ def analysis(name, typ, condition=None, query=None):
 contrast_pst = analysis('presence', 'categorize', condition='present')
 
 # PAS for all trials
-contrast_pst = analysis('pas_all', 'regress', condition='pas')
+contrast_pas = analysis('pas_all', 'regress', condition='pas')
 
 # PAS for present trials only
 regress_pas_pst = analysis('pas_pst', 'regress', condition='pas',
@@ -73,113 +73,80 @@ regress_abs_seen = analysis('abs_seen', 'regress', condition='abs_seen')
 # 5 SOAs compared to absent
 regress_abs_seen = analysis('abs_soa', 'regress', condition='abs_soa')
 
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-# XXX check that SOA is nan if absent, and != from soa_ttl
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-pas_regress_list = list()
-for soa in soas:
-    sub_regress_ = analysis('soa_%s' % soa, 'regress', condition='pas',
-                            query='soa==%s' % soa)
-    pas_regress_list.append(sub_regress_)
-regress_pas_soa = analysis('soa_pas', 'regress', condition=pas_regress_list)
+# regress_pas_list = list()
+# for soa in soas:
+#     sub_regress_ = analysis('soa_%s' % soa, 'regress', condition='pas',
+#                             query='soa==%s' % soa)
+#     pas_regress_list.append(sub_regress_)
+# regress_pas_soa = analysis('soa_pas', 'regress', condition=pas_regress_list)
 
-# Effects of visibility context
+# # Effects of visibility context
 
-# # Global block context (middle SOAs only)
+# Global block context (present, middle SOAs only)
 contrast_block = analysis('block','categorize', condition='block',
                           query='present==True and soa!=17 and soa!=83')
 
-# Global block context, SOA (middle SOAs only)
-contrast_block_soa = list()
+# Global block context, SOA (present, middle SOAs only)
+contrast_block_list = list()
 for soa in mid_soas:
-    sub_contrast_ = analysis('soa_%s' % soa, 'categorize', condition='block',
+    sub_contrast_ = analysis('block_soa_%s' % soa, 'categorize', condition='block',
                         query='soa==%s' % soa)
-    contrast_block_soa.append(sub_contrast_)
+    contrast_block_list.append(sub_contrast_)
 regress_block_soa = analysis('block_soa', 'regress',
-                             condition=contrast_block_soa)
+                             condition=contrast_block_list)
 
-pas_regress_list = list()
-for soa in soas:
-    sub_regress_ = analysis('soa_%s' % soa, 'regress', condition='pas',
-                            query='soa==%s' % soa)
-    pas_regress_list.append(sub_regress_)
-regress_pas_soa = analysis('soa_pas', 'regress', condition=pas_regress_list)
-
-
-# # Global block context, PAS (middle SOA, present only)
-block_contrast_list = list()
+# Global block context, PAS (present, middle SOAs only)
+contrast_block_list = list()
 for pas in range(4):
-    sub_contrast_ = analysis('pas_%s' % pas, 'categorize', condition='block',
-                             query='pas==pas and present==True and soa!=17 and soa!=83')
-    block_contrast_list.append(sub_contrast_)
+    sub_contrast_ = analysis('block_pas_%s' % pas, 'categorize', condition='block',
+                             query='present==True and soa!=17 and soa!=83 and pas==%s' % pas )
+    contrast_block_list.append(sub_contrast_)
 regress_block_pas = analysis('block_pas', 'regress',
-                             condition = block_contrast_list)
+                             condition = contrast_block_list)
 
-# # Global block context, seen/unseen (present, middle SOAs only)
-block_contrast_list = list()
-for block in ['visible', 'invisible']:
-    sub_contrast_ = analysis('seen_'+block[:-4], 'categorize', condition='seen',
-                             query='present==True and soa!=17 and soa!=83 and block=="%s"' % block)
-    block_contrast_list.append(sub_contrast_)
+# Global block context, seen/unseen (present, middle SOAs only)
+contrast_block_list = list()
+for seen in ['seen', 'unseen']:
+    sub_contrast_ = analysis('block_%s' % seen, 'categorize', condition='block',
+                             query='present==True and soa!=17 and soa!=83 and seen==%s' % seen)
+    contrast_block_list.append(sub_contrast_)
 contrast_block_seen = analysis('block_seen','categorize',
-                               condition = block_contrast_list)
+                               condition = contrast_block_list)
 
 # Local N-1 context (all trials)
 contrast_local = analysis('local','categorize', condition = 'local_context')
 
+# Local N-1 context, SOA (all trials)
+contrast_local_list = list()
+for soa in soas:
+    sub_contrast_ = analysis('local_%s' % soa, 'categorize',
+                             condition = 'local_context',
+                             query='soa_ttl==%s' % soa)
+    contrast_local_list.append(sub_contrast_)
+contrast_local_soa = analysis('local_soa','regress',
+                              condition = contrast_local_list)
 
-# # Local N-1 context, SOA (all trials)
-# contrast_local_soa = list()
-# for soa in soas:
-#     contrast = dict(
-#         name='localS'+str(soa)+'-localU'+str(soa),
-#         operator = evoked_subtract, conditions = [
-#             dict(name='localS'+str(soa), include=dict(local_context='S',soa_ttl=soa)),
-#             dict(name='localU'+str(soa), include=dict(local_context='U',soa_ttl=soa))])
-#     contrast_local_soa.append(contrast)
-# regress_local_soa = dict(
-#     name='regress_local_soa',operator=evoked_spearman,
-#     conditions=contrast_local_soa)
-
-
-
-
-
-# # Local N-1 context, PAS (all trials)
-# contrast_local_pas = list()
-# for pas in range(4):
-#     contrast = dict(
-#         name='localSpas'+str(pas) + '-localUpas'+str(pas),
-#         operator = evoked_subtract, conditions = [
-#             dict(name='localSpas'+str(pas), include=dict(local_context='S', pas=pas)),
-#             dict(name='localUpas'+str(pas), include=dict(local_context='U', pas=pas))])
-#     contrast_local_pas.append(contrast)
-# regress_local_pas = dict(
-#     name='regress_local_pas', operator=evoked_spearman,
-#     conditions=contrast_local_pas)
-
-regress_list = list()
-for local in ['S', 'U']:
-    regress = analysis('pas_local_%s' % local, 'regress', condition='pas',
-                       query='local_context=="%s"' % local)
-    regress_list.append(regress)
-contrast_local_pas = analysis('contrast_local_pas', 'categorize',
-                              condition=regress_list)
-
+# Local N-1 context, PAS (all trials)
+contrast_local_list = list()
+for pas in range(4):
+    sub_contrast_ = analysis('local_pas_%s' % soa, 'categorize',
+                             condition = 'local_context', query='pas==%s' % pas)
+    contrast_local_list.append(sub_contrast_)
+contrast_local_pas = analysis('local_pas','regress',
+                              condition = contrast_local_list)
 
 # Local N-1 context, seen/unseen (all trials)
-contrast_list = list()
-for local in ['S', 'U']:
-    contrast = analysis('seen_local_%s' % local, 'categorize',
-                        condition='seen', query='local_context=="%s"' % local)
-    contrast_list.append(contrast)
-contrast_local_seen = analysis('contrast_local_seen', 'categorize',
-                               condition=contrast_list)
+contrast_local_list = list()
+for seen in ['seen', 'unseen']:
+    sub_contrast_ = analysis('local_%s' % seen, 'categorize',
+                             condition='local_context', query='seen==%s' % seen)
+    contrast_local_list.append(sub_contrast_)
+contrast_local_seen = analysis('local_seen', 'categorize',
+                               condition=contrast_local_list)
 
-## Control contrasts
+# # Control contrasts
 
 # Top vs. bottom: Cannot do because not coded in triggers
-contrast_local = analysis('local','categorize', condition = 'local_context')
 
 # Letter vs. digit stimulus (all present trials)
 contrast_target = analysis('target','categorize', condition = 'target')
