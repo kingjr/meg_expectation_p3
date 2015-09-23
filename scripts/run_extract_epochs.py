965bcfe7f4aa62a87b8b4dc975857b50bd731c9f
Gabriela Meade <meade.gabriela@gmail.com>
2015-09-23 09:23:07 -0700

    add artefact rejection (and comment plotting because causes error)

--- scripts/run_extract_epochs.py
+++ scripts/run_extract_epochs.py
@@ -6,6 +6,7 @@ import sys
 import os.path as op
 import numpy as np
 import mne
+import matplotlib.pyplot as plt
 from mne.io import Raw
 from mne.io.pick import _picks_by_type as picks_by_type # XXX users should not need this
 from mne.preprocessing import read_ica
@@ -13,7 +14,9 @@ from mne.viz import plot_drop_log
 
 from meeg_preprocessing.utils import setup_provenance, set_eog_ecg_channels
 
-from p300.conditions import events_select_condition
+from p300.conditions import get_events, events_select_condition
+
+from scripts.wip_artefacts import artefact_rej
 
 from scripts.config import (
     data_path,
@@ -26,6 +29,7 @@ from scripts.config import (
     events_fname_filt_tmp,
     chan_types,
     epochs_params,
+    epochs_types,
     use_ica,
     open_browser
 )
@@ -61,9 +65,9 @@ for subject in subjects:
         # Set ECG EOG channels XXX again?
         set_eog_ecg_channels(raw, eog_ch=eog_ch, ecg_ch=ecg_ch)
 
-        # Interpolate bad channels
-        if 'eeg' in [i['name'] for i in chan_types] and len(raw.info['bads']) > 0:
-            raw.interpolate_bad_channels()
+        # # Interpolate bad channels
+        # if 'eeg' in [i['name'] for i in chan_types] and len(raw.info['bads']) > 0:
+        #     raw.interpolate_bad_channels()
 
         # Get events identified in run_extract_events
         events = mne.read_events(
@@ -99,45 +103,47 @@ for subject in subjects:
             # Append runs
             epochs_list.append(epochs)
 
-
-    # Save and report
     for name, epochs_list in zip([ep['name'] for ep in epochs_params], all_epochs):
         # Concatenate runs
         epochs = mne.epochs.concatenate_epochs(epochs_list)
+
+        # Artefact rejection (EEG channels then trials)
+        epochs = artefact_rej(name,subject,epochs)
+
         # Save
         epochs.save(op.join(this_path, '{}-{}-epo.fif'.format(name, subject)))
 
-        # Plot
-        #-- % dropped
-        report.add_figs_to_section(
-            plot_drop_log(epochs.drop_log), '%s (%s): total dropped'
-                          % (subject, name), 'Drop: ' + name)
-        #-- % trigger channel
-        ch = mne.pick_channels(epochs.ch_names, 'STI101')
-        epochs._data[:, ch,:] = (2.*(epochs._data[:,ch,:] > 1) +
-                                 1.*(epochs._data[:,ch,:] < 8192))
-
-        fig = mne.viz.plot_image_epochs(epochs, ch, scalings=dict(stim=1),
-                                        units=dict(stim=''), vmin=1, vmax=3,
-                                        show=False)
-        report.add_figs_to_section(fig, '%s (%s): triggers' % (subject, name),
-                                   'Triggers: ' + name)
-
-        #-- % evoked
-        evoked = epochs.average()
-        fig = evoked.plot(show=False)
-        report.add_figs_to_section(fig, '%s (%s): butterfly' % (subject, name),
-                                   'Butterfly: ' + name)
-        times = np.arange(epochs.tmin, epochs.tmax,
-                          (epochs.tmax - epochs.tmin) / 20)
-        for ch_type in chan_types:
-            ch_type = ch_type['name']
-            if ch_type in ['eeg', 'meg', 'mag']:
-                if ch_type == 'meg':
-                    ch_type = 'mag'
-            fig = evoked.plot_topomap(times, ch_type=ch_type)
-            report.add_figs_to_section(
-                fig, '%s (%s): topo %s' % (subject, name, ch_type),
-                                        'Topo: ' + name)
+        # # Plot
+        # #-- % dropped
+        # report.add_figs_to_section(
+        #     plot_drop_log(epochs.drop_log), '%s (%s): total dropped'
+        #                   % (subject, name), 'Drop: ' + name)
+        # #-- % trigger channel
+        # ch = mne.pick_channels(epochs.ch_names, 'STI101')
+        # epochs._data[:, ch,:] = (2.*(epochs._data[:,ch,:] > 1) +
+        #                          1.*(epochs._data[:,ch,:] < 8192))
+        #
+        # fig = mne.viz.plot_image_epochs(epochs, ch, scalings=dict(stim=1),
+        #                                 units=dict(stim=''), vmin=1, vmax=3,
+        #                                 show=False)
+        # report.add_figs_to_section(fig, '%s (%s): triggers' % (subject, name),
+        #                            'Triggers: ' + name)
+        #
+        # #-- % evoked
+        # evoked = epochs.average()
+        # fig = evoked.plot(show=False)
+        # report.add_figs_to_section(fig, '%s (%s): butterfly' % (subject, name),
+        #                            'Butterfly: ' + name)
+        # times = np.arange(epochs.tmin, epochs.tmax,
+        #                   (epochs.tmax - epochs.tmin) / 20)
+        # for ch_type in chan_types:
+        #     ch_type = ch_type['name']
+        #     if ch_type in ['eeg', 'meg', 'mag']:
+        #         if ch_type == 'meg':
+        #             ch_type = 'mag'
+        #     fig = evoked.plot_topomap(times, ch_type=ch_type)
+        #     report.add_figs_to_section(
+        #         fig, '%s (%s): topo %s' % (subject, name, ch_type),
+        #                                 'Topo: ' + name)
 
 report.save(open_browser=open_browser)
