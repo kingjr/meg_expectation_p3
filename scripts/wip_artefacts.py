e705976d8c581e8d2c6cc33a11f3635711487c3f
Gabriela Meade <meade.gabriela@gmail.com>
2015-10-20 21:36:36 -0700

    clean up, separate plotting fuction, but still need to double check

--- scripts/wip_artefacts.py
+++ scripts/wip_artefacts.py
@@ -5,29 +5,21 @@ import mne
 from meeg_preprocessing.utils import setup_provenance
 # from mne.datasets import sample
 from p300.conditions import get_events
-from scripts.config import (data_path, epochs_params, subjects, epochs_types,
+from scripts.config import (data_path, epochs_params, epochs_types,
                             results_dir,open_browser)
+from jr.stats import robust_mean
 
 report, run_id, results_dir, logger = setup_provenance(script=__file__,
                                                        results_dir=results_dir)
 
-# epoch_params = epochs_params[0]
-# epoch_type = epochs_types[0
-#for subject in subjects:
-
 def artefact_rej(eptyp_name,subject,epochs):
     print('Artefact Rejection')
-    # Extract events from mat file
-    # bhv_fname = op.join(data_path, 'behavior',
-    #                     '{}_behaviorMEG.mat'.format(subject[-2:]))
-    #eptyp_name = epoch_params['name'] + epoch_type
-    # Get MEG data
-    # epo_fname = op.join(data_path, 'MEG', subject,
-    #                     '{}-{}-epo.fif'.format(eptyp_name, subject))
-    # epochs = mne.read_epochs(epo_fname)
     epochs_ = epochs.copy()
+    # Only define artefact rejection based on this shorter epoch
+    # (but keep the whole trial)
     epochs_.crop(-.200, .600)
 
+    # XXX Specific to Gabriela's data
     # XXX probably move this correction before saving the epoch data
     sel_ch = np.where([ch == 'EEG064' for ch in epochs_.ch_names])[0]
     if len(sel_ch):
@@ -38,9 +30,7 @@ def artefact_rej(eptyp_name,subject,epochs):
     events = get_events(epochs_.events)
 
     # Identify bad EEG channels
-    # XXX Specific to Gabriela's data XXX I think this refers to EEG064, not this, right?
     epochs_eeg = epochs_.pick_types(meg=False, eeg=True, copy=True)
-    #evoked_bad = epochs_eeg.average() #to plot before
     data = epochs_eeg._data
     chan_deviation = np.median(np.std(data, axis=2), axis=0)
 
@@ -49,43 +39,13 @@ def artefact_rej(eptyp_name,subject,epochs):
         center = np.median(x, axis=axis)
         return np.median(np.abs(x - center), axis=axis)
 
-    threshold = np.median(chan_deviation) + 10 * mad(chan_deviation)
-    # fig, ax = plt.subplots(1)
-    # ax.plot(chan_deviation)
-    # ax.axhline(threshold, color='r')
-    # report.add_figs_to_section(fig, ('%s (%s): Artifact thresholding' % (
-    #      subject, eptyp_name)))
-    bad_channels = np.where(chan_deviation > threshold)[0]
-    #plt.show()
-
-    # Interpolate
-    # epochs_eeg.info['bads'] += [epochs_eeg.ch_names[ch] for ch in bad_channels]
-    # epochs_eeg.interpolate_bads(reset_bads=False) #eeg only to plot
-    # evoked_eeg = epochs_eeg.average()
+    # Define the threshold, find bad channels
+    chan_threshold = np.median(chan_deviation) + 10 * mad(chan_deviation)
+    bad_channels = np.where(chan_deviation > chan_threshold)[0]
 
     # Correct uncropped data
     epochs.info['bads'] += [epochs_eeg.ch_names[ch] for ch in bad_channels]
     epochs.interpolate_bads(reset_bads=False)
-
-    # check
-    # evoked_bad = epochs_eeg.average()
-    # epochs_eeg.info['bads'] = [epochs_eeg.ch_names[ch] for ch in bad_channels]
-    # epochs_eeg.interpolate_bads(reset_bads=False)
-    # evoked_good = epochs_eeg.average()
-    # evoked_bad.plot(show=False)
-    # evoked_good.plot(show=False)
-    # evoked_bad.plot_topomap(show=False)
-    # evoked_good.plot_topomap()
-
-    # fig1 = evoked_bad.plot_topomap(show=False)
-    # report.add_figs_to_section(fig1, ('%s (%s): BEFORE artifact rejection' % (
-    #     subject, eptyp_name)))
-    # fig2 = evoked_eeg.plot_topomap(show=False)
-    # report.add_figs_to_section(fig2, ('%s (%s): AFTER artifact rejection' % (
-    #     subject, eptyp_name)))
-    #
-
-
 
     # Identify bad trials
     # --- normalize all channel types
@@ -108,20 +68,43 @@ def artefact_rej(eptyp_name,subject,epochs):
     pick = mne.pick_types(epochs_.info, eeg=True, meg=True)
     data = epochs_norm._data
     trial_deviation = np.median(np.std(data, axis=2), axis=1)
-    threshold = np.median(trial_deviation) + 10 * mad(trial_deviation)
-    # fig3, ax = plt.subplots(1)
-    # ax.plot(trial_deviation)
-    # ax.axhline(threshold, color='r')
-    # report.add_figs_to_section(fig3, ('%s (%s): Trial Rejection' % (
-    #      subject, eptyp_name)))
-    good_trials = np.where(trial_deviation < threshold)[0]
+    trial_threshold = np.median(trial_deviation) + 10 * mad(trial_deviation)
+
+    # Find the good trials, only keep those
+    good_trials = np.where(trial_deviation < trial_threshold)[0]
     epochs=epochs[good_trials]
 
-#report.save(open_browser=open_browser)
+    # Plot
+
+    #TODO need to change to double check these plots, add to return statement?
+
+    # Channel rejection check
+    # fig, ax = plt.subplots(1)
+    # ax.plot(chan_deviation)
+    # ax.axhline(chan_threshold, color='r')
+    # report.add_figs_to_section(fig, ('%s (%s): Artifact thresholding' % (
+    #      subject, eptyp_name)))
+
+    # Topo from before channel rejection
+    # evoked_bad.data = robust_mean(epochs_eeg._data, axis = 0)
+    # fig1 = evoked_bad.plot_topomap(show=False)
+    # report.add_figs_to_section(fig1, ('%s (%s): BEFORE artifact rejection' % (
+    #     subject, eptyp_name)))
+
+    # Topo after channel rejection
+    # Interpolate EEG only (just for plotting)
+    # epochs_eeg.info['bads'] += [epochs_eeg.ch_names[ch] for ch in bad_channels]
+    # epochs_eeg.interpolate_bads(reset_bads=False)
+    # evoked_good = robust_mean(epochs_eeg._data, axis=0)
+    # fig2 = evoked_good.plot_topomap(show=False)
+    # report.add_figs_to_section(fig2, ('%s (%s): AFTER artifact rejection' % (
+    #     subject, eptyp_name)))
+
+    # Trial rejection check
+    # fig3, ax = plt.subplots(1)
+    # ax.plot(trial_deviation)
+    # ax.axhline(trial_threshold, color='r')
+    # report.add_figs_to_section(fig3, ('%s (%s): Trial Rejection' % (
+    #      subject, eptyp_name)))
 
     return epochs  # [fig1, fig2, ]
-
-    #
-    # # TODO put this into a function and relaunch the mask
-    # # subtraction and the basic univariate topographies
-    # # Later: add the ICA correction: maybe for later, not now
