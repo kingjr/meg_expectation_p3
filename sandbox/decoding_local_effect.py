fff49f0d4360ddee87d1327ce2253874d729458a
kingjr <jeanremi.king+github@gmail.com>
2016-02-03 17:53:41 -0500

    FIX: decode multiple factor

--- sandbox/decoding_local_effect.py
+++ sandbox/decoding_local_effect.py
@@ -8,6 +8,7 @@ from scripts.config import epochs_params, subjects
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Ridge, RidgeCV
+from sklearn.cross_validation import KFold
 
 # File name
 from scripts.config import data_path
@@ -30,11 +31,12 @@ for subject in subjects:
     epochs.crop(0, .7)
 
     # Apply each analysis
-    sel = events.query('soa > 17 and soa < 83 and not local_undef' +
+    sel = events.query('soa > 17 and soa < 83 and not local_undef ' +
                        'and not pas_undef').index
     single_factor = False
     if single_factor:
-        y = np.array(events['local_seen'], float)
+        factors = ['local_seen']
+        y = np.array(events[factors], float)
         # when we predict only one factor, we can use the LR
         clf = None  # by default: StandardScaler + LogisticRegression
         scorer = scorer_auc
@@ -43,13 +45,17 @@ for subject in subjects:
                                        n_jobs=-1)
         gat.fit(epochs[sel], y=y[sel])
         scores = gat.score(epochs[sel], y=y[sel])
+        factors_scores = [scores]
     else:
         factors = ['local_seen', 'pas', 'soa']
         y = np.array(events[factors].values, float)
         # we have to use another regression: Ridge should be ok
         # RidgeCV(alphas=[.01, .1, 1., 10])  # better but slower
         clf = make_pipeline(StandardScaler(), Ridge())
-        gat = GeneralizationAcrossTime(clf=clf, scorer=scorer_spearman,
+        # We need to specify the cross validation because it cannot be
+        # stratified anymore.
+        cv = KFold(len(y[sel]))
+        gat = GeneralizationAcrossTime(clf=clf, scorer=scorer_spearman, cv=cv,
                                        n_jobs=-1)
         # fits all factors at onces
         gat.fit(epochs[sel], y=y[sel])
@@ -58,7 +64,15 @@ for subject in subjects:
         # score each factor separately
         factors_scores = list()
         for ii, factor in enumerate(factors):
-            gat.scores_ = y_pred[:, :, :, ii]  # train time, test time, trials, factor
-            scores = gat.score(y=y[sel])
+            # y_pred shape: train time, test time, trials, factor
+            gat.scores_ = y_pred[:, :, :, ii]
+            scores = gat.score(y=y[sel, ii])
             factors_scores.append(scores)
-    all_scores.append(scores)
+    all_scores.append(factors_scores)
+
+for ii, this_factor in enumerate(factors):  # for each factor
+    # mean beta coefficient across subjects
+    gat.scores_ = np.mean([sbj_factor_score[ii]
+                           for sbj_factor_score in all_scores],
+                          axis=0)
+    gat.plot()
