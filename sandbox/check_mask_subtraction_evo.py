2fb3ed9d29f305d8341cd2a8b9993ef3167c5b96
Gabriela Meade <meade.gabriela@gmail.com>
2016-03-01 15:40:51 -0800

    plot with robust mean mask template and with normal mean mask template

--- sandbox/check_mask_subtraction_evo.py
+++ sandbox/check_mask_subtraction_evo.py
@@ -10,33 +10,40 @@ from p300.run_mask_subtraction import subtract_mask
 from scripts.config import subjects
 from mne import pick_types
 
-mask_list, unmask_list = list(), list()
+mask_list, unmask_list, mean_all_unmask_list = list(), list(), list()
 for subject in subjects:
     print subject
     path = '/Volumes/INSERM/data/MEG/%s/' % subject
-    fname_mask = path + 'stim_lock-%s-epo.fif' % subject
-    fname_unmask = path + 'stim_lock-unmasked-%s-epo.fif' % subject
+    fname_mask = path + 'nobl-stim_lock-%s-epo.fif' % subject
+    fname_unmask = path + 'nobl-stim_lock-unmasked-%s-epo.fif' % subject
+    fname_mean_all_unmask = path + 'MEAN_ALL-nobl-stim_lock-unmasked-%s-epo.fif' % subject
 
     epochs_mask = mne.read_epochs(fname_mask)
     epochs_unmask = mne.read_epochs(fname_unmask)
+    epochs_mean_all_unmask = mne.read_epochs(fname_mean_all_unmask)
 
     events = get_events(epochs_mask.events)
     absent = np.where(events['present'] == False)[0]
     evoked_mask = epochs_mask[absent].average()
     evoked_unmask = epochs_unmask[absent].average()
+    evoked_mean_all_unmask = epochs_unmask[absent].average()
 
     #Only take the MEG channels (because diff # of EEG channels/subj)
     meg = pick_types(evoked_mask.info, eeg=False, meg=True)
     evoked_mask.data=evoked_mask.data[meg,:]
     evoked_unmask.data=evoked_unmask.data[meg,:]
+    evoked_mean_all_unmask.data=evoked_unmask.data[meg,:]
 
     mask_list.append(evoked_mask.data)
     unmask_list.append(evoked_unmask.data)
+    mean_all_unmask_list.append(evoked_unmask.data)
 
 evoked_mask.data = np.mean(mask_list, axis=0)
 evoked_unmask.data = np.mean(unmask_list, axis=0)
+evoked_mean_all_unmask.data = np.mean(mean_all_unmask_list, axis=0)
 plt.matshow(evoked_mask.data)
 plt.matshow(evoked_unmask.data)
+plt.matshow(evoked_mean_all_unmask.data)
 # evoked_mask.plot(show=False)
 # evoked_unmask.plot(show=False)
 plt.show()
